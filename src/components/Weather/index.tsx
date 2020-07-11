158fa168fe4c5c8d980264c74c872b97e0e80967
Shaun Lewin <info@shaunlewin.com>
2020-07-10 22:46:50 -0400

    getWeatherData() works!

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -11,33 +11,42 @@ type WeatherProps = {
 
 type WeatherWidgetState = {
     zip: string,
-    forecast: {},
-    scale: TemperatureScale
+    forecast: any,
+    units: TemperatureScale
 };
 
 class Weather extends React.Component<WeatherProps, WeatherWidgetState> {
 
     state: WeatherWidgetState = {
         zip: '',
-        forecast: {},
-        scale: TemperatureScale.Fahrenheit
+        forecast: null,
+        units: TemperatureScale.Fahrenheit
     };
 
-    componentDidMount(): void {
+    componentWillMount(): void {
         const {zip} = this.props;
         this.getWeatherForecast(zip);
     }
 
     async getWeatherForecast(zip: string) {
         if(zip.length !== 5) return;
+        const { units } = this.state;
 
         const OPEN_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/forecast';
         const API_KEY = '74d9bc317f6cb40688304324e3555d46';
 
-        fetch(OPEN_WEATHER_API_URL + `?zip=${zip},us&appid=${API_KEY}`);
+        let requestForecastUrl = OPEN_WEATHER_API_URL + `?zip=${zip},us&appid=${API_KEY}`;
+        requestForecastUrl += '&' + ((units === TemperatureScale.Fahrenheit) ? 'units=imperial': 'units=metric');
+
+        let forecast = await fetch(requestForecastUrl);
+        forecast = await forecast.json();
+
+        this.setState({forecast});
     }
 
     render() {
+        const { forecast } = this.state;
+
         return (
             <div className="weather-widget">
                 <div className="weather-widget__container">
@@ -53,6 +62,10 @@ class Weather extends React.Component<WeatherProps, WeatherWidgetState> {
                         <div className="forecast">
                             <div className="forecast__container">
                                 <h1>Forecast</h1>
+                                {forecast
+                                    ? <h2>{forecast.city.name}</h2>
+                                    : <h2>no</h2>
+                                }
                             </div>
                         </div>
                     </form>
